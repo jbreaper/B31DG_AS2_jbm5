993455be5b9bdf24f74a7cec62b7f22e94728aa3
jbreaper <jbmavor@hotmail.co.uk>
2022-02-15 14:10:03 +0000

    basic setup

---
+++ em_as2_jbm5/em_as2_jbm5.ino
@@ -0,0 +1,46 @@
+
+#define hz_to_ms(hz) ((1/hz)*1000)
+
+void setup(){
+    pinMode();
+}
+
+void task_1(){
+    delayMicroseconds(50);
+}
+
+void task_2(){
+
+}
+
+void task_3(){
+
+}
+
+void task_4(){
+
+}
+
+void task_5(){
+
+}
+
+void task_6(){
+
+}
+
+void task_7(){
+
+}
+
+void task_8(){
+
+}
+
+void task_9(){
+
+}
+
+void loop(){
+    
+}
